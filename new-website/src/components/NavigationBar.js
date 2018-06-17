12648b3831cd9b121e67660c73f0bf0b018b0588
ledet20 <ledet2016@gmail.com>
2018-06-17 08:32:32 -0500

    npm module for nav bar downloaded

---
+++ new-website/src/components/NavigationBar.js
@@ -0,0 +1,28 @@
+import React  from 'react';
+import { Navbar , Nav, NavItem } from 'react-bootstrap';
+import Navigation from '../stylesheets/Navigation.css';
+
+export const NavigationBar = () => {
+
+    return (
+    <Navbar fluid >
+   <Navbar.Header>
+    <Navbar.Brand>
+      <a href="#home">LA</a>
+    </Navbar.Brand>
+  </Navbar.Header>
+  <Nav>
+    <NavItem eventKey={1} href="#" className="nav">
+      ABOUT
+    </NavItem>
+    <NavItem eventKey={2} href="#" className="nav">
+      BLOG
+    </NavItem>
+    <NavItem eventKey={2} href="#" className="nav">
+      CONTACT
+    </NavItem>
+    
+   </Nav> 
+  </Navbar> 
+    );
+}
