c11bbe09893a17298b571f11fbaaef458678725b
ledet20 <ledet2016@gmail.com>
2018-07-22 15:24:43 -0500

    contact page complete

--- new-website/src/components/Contact.js
+++ new-website/src/components/Contact.js
@@ -7,13 +7,15 @@ import phone from '../images/phone-call.png';
 import twitter from '../images/twitter.png';
 
 export const Contact = () => (
-    <div className='contact'>
+    <div className='contactPage'>
         <p> LET'S CONNECT </p>
-        {/* <div className='contactIcon'>  <img className='icon' src={linkedin}>    </img> </div> */}
-        {/* <div className='contactIcon'> <img className='icon' src={message}>    </img> </div>
-        <div className='contactIcon'> <img className='icon' src={resume}>   </img> </div>
-        <div className='contactIcon'> <img className='icon' src={phone}>  </img> </div>
-        <div className='contactIcon'> <img className='icon' src={twitter}>  </img> </div> */}
+    <div className='contact' >
+        <div className='contactIcon'> <a href='https://www.linkedin.com/in/ledet-awano/' target='_blank'> <img className='icon' src={linkedin} />  </a> </div> 
+        <div className='contactIcon'> <a href='mailto:ledet2016@gmail.com' target='_blank' > <img className='icon' src={message} /> </a> </div>
+        <div className='contactIcon'> <a href='https://resume.creddle.io/resume/d9t8szk4owu' target='_blank' > <img className='icon' src={resume} /> </a> </div>
+        <div className='contactIcon'> <a href='tel:2145664108' target='_blank' > <img className='icon' src={phone} /> </a> </div>
+        <div className='contactIcon'> <a href='https://twitter.com/awnn111' target='_blank' > <img className='icon' src={twitter} /> </a> </div>
+    </div>
     </div>
 )
 
--- new-website/src/components/Projects.js
+++ new-website/src/components/Projects.js
@@ -8,10 +8,40 @@ import distrk from '../images/disTrkG.jpg';
 export const Projects = () => (
     <div className='projects'>
         <p className='myProjects'> MY LATEST PROJECTS </p> 
-        <div className='project'> <img   className='img' src={puzzle}></img></div>
-        <div className='project'> <img className='img' src={arcangel}></img></div>
-        <div className='project'> <img className='img' src={distrk}></img></div>
+        <div className='project hvrbox'> 
+            <img className='img hvrbox-layer_bottom' src={arcangel} />
+            <div className="hvrbox-layer_top hvrbox-layer_slidedown">
+            <a className='link' href='https://github.com/ledet20/arcangel' target="_blank" >
+		        <div className="hvrbox-text"> Web and mobile application that uses the Twitter API to find people who need help during a natural disaster. Esri gis technology used to place the location of where the most help is needed by a users tweet location.</div>
+            </a>
+	        </div>
+        </div>
+        <div className='hvrbox project'> 
+           <img className='img hvrbox-layer_bottom' src={puzzle} />
+            <div className="hvrbox-layer_top hvrbox-layer_slidedown">
+            <a className='link' href='https://github.com/ledet20/15-puzzles-game' target="_blank" >
+                 <div className="hvrbox-text"> Sliding game puzzle that has four levels of diffuculty, includes top 5 scoreboard and the ability to recieve hints during the duration of the game.</div> 
+            </a>
+	        </div>
+        </div>
+        <div className='project hvrbox'> 
+            <img className='img hvrbox-layer_bottom' src={distrk} />
+            <div className="hvrbox-layer_top hvrbox-layer_slidedown">
+              <a className='link' href='https://github.com/ledet20/CodeRED_2017' target="_blank" >
+		            <div className="hvrbox-text">Web application tool that would make communication between patients and physicians more effective in regards to automating the patient discharge process.</div>
+                </a>
+	        </div>
+         </div>
         <div className='projectLink'> <a className='link' href='https://github.com/ledet20' target="_blank"> VIEW MORE PROJECTS </a> </div>
 
     </div>
 );
+
+
+
+{/* <div class="hvrbox">
+	<img src="img/photos/photo1.jpg" alt="Mountains" class="hvrbox-layer_bottom">
+	<div class="hvrbox-layer_top hvrbox-layer_slidedown">
+		<div class="hvrbox-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor ligula porttitor, lacinia sapien non.</div>
+	</div>
+</div> */}
